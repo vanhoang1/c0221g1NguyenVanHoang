97f16a8c3320470f8bc06708900055671fb254ef
Your Name <you@example.com>
2021-03-05 16:56:51 +0700

    bai7

--- module1/BienVaKieuDuLieu/ThucHanh/khaibaobien.js
+++ module1/BienVaKieuDuLieu/ThucHanh/khaibaobien.js
@@ -8,6 +8,13 @@
     let f = 20.5;
     let b = true;
     let s = 'Hà Nội';
+    let d = prompt("nhập vào a");
+    let c = prompt("nhập vào b");
+    if (d%c==0){
+        alert("a la bội của b");
+    }else {
+        alert("a không là bội của b");
+    }
     document.write('i = ' + i);
     document.write('<br/>');
     document.write('f = ' + f);
