2b731cae835924e5db5a3a353985ba4a1ea57f57
Your Name <you@example.com>
2021-05-18 17:16:31 +0700

    18/5

--- module3/case_study/day_8.sql
+++ module3/case_study/day_8.sql
@@ -36,20 +36,71 @@ begin
     end if;
 end;
 // delimiter ;
-drop procedure if exists sp_2;
 call sp_2(1111,1,1,1);
 -- 25.	Tạo triggers có tên Tr_1 Xóa bản ghi trong bảng HopDong thì hiển thị tổng số lượng bản ghi còn lại có trong bảng HopDong ra giao diện console của database
-create view dem as select count(h.id_hop_dong) from hop_dong h group by h.id_hop_dong;
+-- bỏ
+-- 26.	Tạo triggers có tên Tr_2 Khi cập nhật Ngày kết thúc hợp đồng, cần kiểm tra xem thời gian cập nhật có phù hợp hay không, với quy tắc sau: Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày. Nếu dữ liệu hợp lệ thì cho phép cập nhật
 
 delimiter //
-
-//
-create trigger tr_1 after delete on hop_dong 
+create trigger tr_2 before update on hop_dong 
 for each row 
 begin
-
-end 
+declare a date;
+declare b date;
+set a = new.ngay_lam_hop_dong;
+set b = new.ngay_ket_thuc;
+if(datediff(b,a)<2) then
+set new.id_hop_dong=old.id_hop_dong;
+set new.id_nhan_vien= old.id_nhan_vien;
+set new.id_khach_hang= old.id_khach_hang;
+set new.ngay_lam_hop_dong= old.ngay_lam_hop_dong;
+end if;
+end ;
 // delimiter ;
-drop trigger tr_1 ;
-delete from hop_dong where id_hop_dong =1;
-
+update hop_dong set ngay_lam_hop_dong ='2057-1-1';
+-- 27.	Tạo user function thực hiện yêu cầu sau:
+-- a.	Tạo user function func_1: Đếm các dịch vụ đã được sử dụng với Tổng tiền là > 2.000.000 VNĐ.
+delimiter //
+create function func_1()
+returns int
+deterministic
+begin
+declare f_dem int;
+set	f_dem = (
+select count(id) from (SELECT 
+                h.id_dich_vu AS id 
+            FROM
+                hop_dong h
+            JOIN dich_vu d ON h.id_dich_vu = d.id_dich_vu
+            JOIN chi_tiet_hop_dong ct ON ct.id_hop_dong = h.id_hop_dong
+            JOIN dich_vu_di_kem dvdk ON dvdk.id_dich_vu_di_kem = ct.id_dich_vu_di_kem
+            GROUP BY h.id_dich_vu
+            HAVING SUM(d.chi_phi_thue + ct.so_luong *  COALESCE( dvdk.gia, 0)) > 2000000 ) as t
+);
+return (f_dem);
+end;
+// delimiter ;
+select func_1() as 'dem_dich_vu' ; 
+-- b.	Tạo user function Func_2: Tính khoảng thời gian dài nhất tính từ lúc bắt đầu làm hợp đồng đến lúc kết thúc hợp đồng mà Khách hàng đã thực hiện thuê dịch vụ (lưu ý chỉ xét các khoảng thời gian dựa vào từng lần làm hợp đồng thuê dịch vụ, không xét trên toàn bộ các lần làm hợp đồng). Mã của Khách hàng được truyền vào như là 1 tham số của function này.
+ delimiter //
+create function func_2(f_id int)
+returns int
+deterministic
+begin
+declare f_dem int;
+set	f_dem = (
+SELECT 
+    MAX(DATEDIFF(h.ngay_ket_thuc, h.ngay_lam_hop_dong))
+FROM
+    chi_tiet_hop_dong ct
+        RIGHT JOIN
+    hop_dong h ON ct.id_hop_dong = h.id_hop_dong
+WHERE
+    ct.id_hop_dong IS NOT NULL
+        AND h.id_khach_hang = f_id
+);
+return (f_dem);
+end;
+// delimiter ;
+ select func_2(1) as 'so_ngay';
+-- 28.	Tạo Store procedure Sp_3 để tìm các dịch vụ được thuê bởi khách hàng với loại dịch vụ là “Room” từ đầu năm 2015 đến hết năm 2019 để xóa thông tin của các dịch vụ đó (tức là xóa các bảng ghi trong bảng DichVu) và xóa những HopDong sử dụng dịch vụ liên quan (tức là phải xóa những bản gi trong bảng HopDong) và những bản liên quan khác
