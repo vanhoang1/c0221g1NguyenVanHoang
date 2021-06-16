0008ee2e6f94065f93ad4a65e40d2a7a9f60f672
Your Name <you@example.com>
2021-06-16 11:49:15 +0700

    update

--- module4/01_Spring_MVC_Overview/practice/customer-management-automated-testing/src/main/java/cg/wbd/grandemonstration/service/ProvinceService.java
+++
@@ -1,7 +0,0 @@
-package cg.wbd.grandemonstration.service;
-
-import cg.wbd.grandemonstration.model.Province;
-
-public abstract class ProvinceService extends AbstractEntityServiceImplWithSpringData<Province, Long> {
-
-}
